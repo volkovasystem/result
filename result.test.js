bc59e25f89b3414569649771ab0a52c4b2d4f08f
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2020-09-11 01:06:54 +0800

    Updated module test structure.

--- result.test.js
+++ result.test.js
@@ -1,23 +1,465 @@
 "use strict";
 
+//; @code-space:template-engine:
 const assert = require( "assert" );
+const util = require( "util" );
 
-const strictAssert = assert.strict;
+const strictAssert = (
+	assert
+	.strict
+);
 
-const Result = require( "./result.js" );
+//; @procedure:check-directory:
+const checkDirectory = (
+	async	function checkDirectory( directoryPath ){
+					const fs = require( "fs" );
+					const path = require( "path" );
+
+					const fsAsync = (
+						fs
+						.promises
+					);
+
+						directoryPath
+					=	(
+							path
+							.resolve(
+								(
+									directoryPath
+								)
+							)
+						);
+
+					try{
+						return	(
+									(
+										await	fsAsync
+												.stat(
+													(
+														directoryPath
+													)
+												)
+									)
+									.isDirectory( )
+								);
+					}
+					catch( error ){
+						return	(
+									false
+								);
+					}
+			}
+);
+//; @procedure:check-directory;
+
+//; @procedure:check-file:
+const checkFile = (
+	async	function checkFile( filePath ){
+					const fs = require( "fs" );
+					const path = require( "path" );
+
+					const fsAsync = (
+						fs
+						.promises
+					);
+
+						filePath
+					=	(
+							path
+							.resolve(
+								(
+									filePath
+								)
+							)
+						);
+
+					try{
+						return	(
+									(
+										await	fsAsync
+												.stat(
+													(
+														filePath
+													)
+												)
+									)
+									.isFile( )
+								);
+					}
+					catch( error ){
+						return	(
+									false
+								);
+					}
+			}
+);
+//; @procedure:check-file;
+
+//;	@procedure:get-directory-file-list:
+const getDirectoryFileList = (
+	async	function getDirectoryFileList( directoryPath ){
+				const fs = require( "fs" );
+				const path = require( "path" );
+
+				const fsAsync = (
+					fs
+					.promises
+				);
+
+					directoryPath
+				=	(
+						path
+						.resolve(
+							(
+								directoryPath
+							)
+						)
+					);
+
+				try{
+					return	(
+								await	fsAsync
+										.readdir(
+											(
+												directoryPath
+											),
+
+											(
+												{
+													"withFileTypes": (
+														true
+													)
+												}
+											)
+										)
+							);
+				}
+				catch( error ){
+					return	(
+								[ ]
+							);
+				}
+			}
+);
+//;	@procedure:get-directory-file-list;
+
+//;	@procedure:execute-shell-script:
+const executeShellScript = (
+	async	function executeShellScript( shellScript, moduleDirectoryPath ){
+				const childProcess = require( "child_process" );
+				const path = require( "path" );
+
+				const execAsync = (
+					util
+					.promisify(
+						(
+							childProcess
+							.exec
+						)
+					)
+				);
+
+				if(
+						(
+								typeof
+								moduleDirectoryPath
+							==	"string"
+						)
+
+					&&	(
+								moduleDirectoryPath
+								.length
+							>	0
+						)
+				){
+					moduleDirectoryPath = (
+						path
+						.resolve(
+							(
+								moduleDirectoryPath
+							)
+						)
+					);
+				}
+				else{
+					moduleDirectoryPath = (
+						process
+						.cwd( )
+					);
+				}
+
+				try{
+					const	{
+								stdout,
+								stderr
+							}
+						=	(
+								await	execAsync(
+											(
+												shellScript
+											),
+
+											(
+												{
+													"cwd": (
+														moduleDirectoryPath
+													)
+												}
+											)
+										)
+							);
+
+					return	(
+								{
+									"outputLog": (
+										stdout
+										.trim( )
+									),
+
+									"errorLog": (
+										stderr
+										.trim( )
+									)
+								}
+							);
+				}
+				catch( error ){
+					return	(
+								{
+									"error": (
+										util
+										.inspect(
+											(
+												error
+											)
+										)
+									)
+								}
+							);
+				}
+			}
+);
+//;	@procedure:execute-shell-script;
+
+//;	@procedure:setup-test-directory:
+const SETUP_TEST_DIRECTORY = (
+	async	function SETUP_TEST_DIRECTORY( ){
+				const shellParameterList = (
+					process
+					.argv
+				);
+
+				const DISABLE_SETUP_TEST_DIRECTORY_SHELL_PARAMETER = (
+					"--disableSetupTestDirectory"
+				);
+
+				const DISABLE_SETUP_TEST_DIRECTORY_SHORT_SHELL_PARAMETER = (
+					"--xstd"
+				);
+
+				const disableSetupTestDirectory = (
+						(
+								shellParameterList
+								.includes(
+									(
+										DISABLE_SETUP_TEST_DIRECTORY_SHELL_PARAMETER
+									)
+								)
+							===	true
+						)
+
+					||	(
+								shellParameterList
+								.includes(
+									(
+										DISABLE_SETUP_TEST_DIRECTORY_SHORT_SHELL_PARAMETER
+									)
+								)
+							===	true
+						)
+				);
+
+				if(
+						(
+								disableSetupTestDirectory
+							===	true
+						)
+				){
+					return	(
+								true
+							);
+				}
+
+				return	(
+							await	executeShellScript(
+										(
+											"mkdir .test || true"
+										)
+									)
+						);
+			}
+);
+//;	@procedure:setup-test-directory;
+
+//;	@procedure:clean-test-directory:
+const CLEAN_TEST_DIRECTORY = (
+	async	function CLEAN_TEST_DIRECTORY( ){
+				const shellParameterList = (
+					process
+					.argv
+				);
+
+				const DISABLE_CLEAN_TEST_DIRECTORY_SHELL_PARAMETER = (
+					"--disableCleanTestDirectory"
+				);
+
+				const DISABLE_CLEAN_TEST_DIRECTORY_SHORT_SHELL_PARAMETER = (
+					"--xctd"
+				);
+
+				const disableCleanTestDirectory = (
+						(
+								shellParameterList
+								.includes(
+									(
+										DISABLE_CLEAN_TEST_DIRECTORY_SHELL_PARAMETER
+									)
+								)
+							===	true
+						)
+
+					||	(
+								shellParameterList
+								.includes(
+									(
+										DISABLE_CLEAN_TEST_DIRECTORY_SHORT_SHELL_PARAMETER
+									)
+								)
+							===	true
+						)
+				);
+
+				if(
+						(
+								disableCleanTestDirectory
+							===	true
+						)
+				){
+					return	(
+								true
+							);
+				}
+
+				return	(
+							await	executeShellScript(
+										(
+											"rm -rfv .test || true"
+										)
+									)
+						);
+			}
+);
+//;	@procedure:clean-test-directory;
+//; @code-space:template-engine;
+
+const Result = (
+	require( "./result.js" )
+);
+
+const TEST_RESULT_INSTANCE = (
+	async	function TEST_RESULT_INSTANCE( ){
+				(
+					await	CLEAN_TEST_DIRECTORY( )
+				);
+
+				(
+					await	SETUP_TEST_DIRECTORY( )
+				);
+
+				try{
+					const actualValue = (
+							(
+											Result( )
+											.result
+								instanceof	Result
+							)
+						===	true
+					);
+
+					const testValue = (
+						true
+					);
+
+					strictAssert
+					.equal(
+						(
+							actualValue
+						),
+
+						(
+							testValue
+						),
+
+						(
+							[
+								"#test-result-instance;",
+
+								"test result instance;",
+
+								`must return, ${ testValue };`
+							]
+						)
+					);
+
+					return	(
+								true
+							);
+				}
+				catch( error ){
+					console
+					.error(
+						(
+							error
+						)
+					);
+
+					return	(
+								false
+							);
+				}
+				finally{
+					(
+						await	CLEAN_TEST_DIRECTORY( )
+					);
+				}
+			}
+);
 
 (
-	function( ){
-		strictAssert
-		.equal(
-			(
-							Result( )
-				instanceof	Result
-			),
+	async	function TEST_SCENE_BASIC( ){
+				(
+					await	CLEAN_TEST_DIRECTORY( )
+				);
 
-			true,
+				console
+				.table(
+					(
+						[
+							{
+								"test": (
+									"test result"
+								),
 
-			"Result function call must return an instance of Result."
-		);
-	}
+								"result": (
+									await	TEST_RESULT_INSTANCE( )
+								)
+							}
+						]
+					)
+				);
+
+				(
+					await	CLEAN_TEST_DIRECTORY( )
+				);
+			}
 )( );
